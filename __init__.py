7b5d2b07a7062c86f2b00fea7e1f8611be659770
Sparta403 <127791519+Sparta403@users.noreply.github.com>
2023-03-17 17:04:03 +0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,19 +1,3 @@
-from json import loads
-from requests import get
-
-__title__ = 'BSAmino'
-__author__ = 'SFAH'
-__license__ = 'Kratos'
-__copyright__ = 'Copyright 2021-2022 AKATSKI'
-__version__ = '1.1'
-
-from .BSamino import *
-from .utils import *
-
-
-__newest__ = loads(get("https://t.me/spi22der").text)["info"]["version"]
-
-if __version__ != __newest__:
-    print(f"New version of {__title__} available: {__newest__} (Using {__version__})")
-else:
-    print(f"version : {__version__}")
+from .headers import *
+from .objects import *
+from .exception import *
